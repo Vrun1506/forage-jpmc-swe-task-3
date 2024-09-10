08c2c39db26451cc540b84948d96960e66493acd
Varun Nayak <varunnayak@Varuns-MacBook-Pro-2.local>
2024-09-11 00:35:40 +0100

    Finish Task 3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,63 @@
+// This file is responsible for processing the raw stock data we receive from the server before the Graph component renders it.
+
+
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: Date;
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined
 }
 
+// This change is necessary because it determines the structure of the object returned by the generateRow function
+// This return object must correspond to the schema of the table in the Graph component
 
+
+
+
+
+
+
+// We have to change the implementation of the function  to properly process raw server data.
+// First, we’ll compute price_abc and price_def properly (recall what you did back in task 1). 
+// Afterwards we’ll compute the ratio using both prices, set lower and upper bounds, and determine trigger_alert.
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverRespond: ServerRespond[]): Row {
+      const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+      const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+      const ratio = priceABC / priceDEF;
+      const upperBound = 1 + 0.05;
+      const lowerBound = 1 - 0.05;
+
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+          price_abc: priceABC,
+          price_def: priceDEF,
+          ratio,
+          timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+              serverRespond[0].timestamp : serverRespond[1].timestamp,
+          upper_bound: upperBound,
+          lower_bound: lowerBound,
+          trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
       };
-    })
   }
 }
+
+
+// Observe how we’re able to access serverRespond as an array wherein the first element is stock ABC and the second element is stock DEF
+
+
+// Also note how the return value is changed from an array of Row objects to just a single Row object 
+//This change explains why we also adjusted the argument we passed to table.update in Graph.tsx earlier so that consistency is preserved.
+
+
+// The upper_bound and lower_bound are pretty much constant for any data point. 
+//This is how we will be able to maintain them as steady upper and lower lines on the graph. 
+//While 1.05 and 0.95 isn’t really +/-10% of the 12 month historical average ratio (i.e. 1.1 and 0.99) you’re free to play around with the values and see which has a more conservative alerting behavior.
+
+
+// The trigger_alert field is pretty much just a field that has a value (e.g. the ratio) if the threshold is passed by the ratio. 
+//Otherwise if the ratio remains within the threshold, no value/undefined will suffice.
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -22,11 +22,14 @@ class Graph extends Component<IProps, {}> {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
-    const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+    const schema = {  // We want to compare and track the ratios between the two stock prices rather than just comparing the top bid prices. 
+      price_abc: "float",
+      price_def: "float",
+      ratio: "float",
       timestamp: 'date',
+      upper_bound: "float",
+      lower_bound: "float",
+      trigger_alert: "float"
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -38,23 +41,40 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('view', 'y_line');
       elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+
+//● ‘view’ is the kind of graph we want to visualize the data with. Initially, this is already set to y_line.
+// ‘column-pivots’ used to exist and was what allowed us to distinguish / split stock ABC with DEF back in task 2. We removed this because we’re concerned about the ratios between the two stocks and not their separate prices
+// ‘row-pivots’ takes care of our x-axis. This allows us to map each datapoint based on its timestamp. Without this, the x-axis would be blank.
+// ‘columns’ is what allows us to focus on a particular part of a datapoint’s data along the y-axis. Without this, the graph would plot all the fields and values of each datapoint, yielding significant noise. We want to track ratio, lower_bound, upper_bound and trigger_alert.
+// ‘aggregates’ allows us to handle the duplicate data we observed in task 2 and consolidate them into one data point. In our case we only want to consider a data point unique if it has a timestamp. Otherwise, we’ll average the values of the other non-unique fields these ‘similar’ data points share before treating them as one (e.g. ratio, price_abc, ...)
+
+// We are editing the schema to configure the graph.
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
+          price_abc: 'avg',
+          price_def: 'avg',
+          ratio: 'avg',
+          timestamp: 'distinct count',
+          upper_bound: 'avg',
+          lower_bound: 'avg',
+          trigger_alert: 'avg',
       }));
+
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ] as unknown as TableData);
     }
   }
 }
+
+// Finally, we have to make a slight update to the componentDidUpdate method. 
+//This is another component lifecycle method that gets executed whenever the component updates, i.e. when the graph gets new data. 
+//The change we want to make is on the argument we put in this.table.update.
+
 
 export default Graph;
